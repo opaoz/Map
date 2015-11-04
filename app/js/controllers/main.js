0e4e0638f0351592ce5ca76a3fd5f624c666b7dd
Vladimir Levin <Vladimir_Levin@epam.com>
2015-11-04 11:10:19 +0300

    Added demonstration mechanism

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -8,9 +8,28 @@
 (function () {
     angular.module('App').controller('MainController', ['httpRequest', function (httpRequest) {
         var vm = this;
-        console.log('create controller');
-        httpRequest.send('PREFIX rela: <http://purl.org/vocab/relationship/> SELECT ?child ?parent WHERE {?child rela:childOf ?parent.}').then(function (response) {
-            console.log(response);
-        });
+
+        vm._init_ = function () {
+            console.log('create controller');
+            vm.queries = [
+                {
+                    text: 'PREFIX rela: <http://purl.org/vocab/relationship/> SELECT ?child ?parent WHERE {?child rela:childOf ?parent.}'
+                },
+                {
+                    text: ''
+                },
+                {
+                    text: ''
+                }
+            ];
+            vm.output = 'Text will be here';
+        };
+
+        vm.send = function (index) {
+            httpRequest.send(vm.queries[index].text).then(function (response) {
+                console.log(response);
+                vm.output = response.data;
+            });
+        };
     }]);
 })();
---
+++ app/js/filters/code-formatter.js
@@ -0,0 +1,28 @@
+'use strict';
+/**
+ * @file code-formatter.js
+ *
+ * @author Vladimir_Levin
+ * @date 04.11.2015
+ */
+(function () {
+    angular.module('App').filter('codeFormatter', function () {
+        return function (value) {
+            if (angular.isObject(value) || angular.isArray(value)) {
+                value = angular.toJson(value);
+            }
+
+            if (!angular.isString(value)) {
+                return value;
+            }
+
+            return value.replace(/(\[|,|\{)/g, '$1\n')
+                .replace(/]/g, '\n]')
+                .replace(/"(parent|child)"/g, '\t\t<code style="color:blue;">$1</code>')
+                .replace(/"http:\/\/family\/([a-z]+)"/g, '<code style="color:maroon">"$1"</code>')
+                .replace(/\{/g, '\t{')
+                .replace(/}/g, '\n\t}');
+        };
+    });
+
+})();
