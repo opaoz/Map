a90c81d5760a0aa8f631d2317c0928d8dc3f8b20
Vladimir Levin <Vladimir_Levin@epam.com>
2016-02-01 12:29:43 +0300

    Return to maps

--- app/js/controllers/main.js
+++ app/js/controllers/main.js
@@ -6,34 +6,41 @@
  * @date 12.10.2015
  */
 (function () {
-    angular.module('App').controller('MainController', ['httpRequest', function (httpRequest) {
+    angular.module('App').controller('MainController', ['config', '_', function (config, _) {
         var vm = this;
 
         vm._init_ = function () {
-           /* console.log('create controller');
-            vm.queries = [
-                {
-                    title:'Parent',
-                    text: 'SELECT ?child ?parent WHERE {?child rela:childOf ?parent.}'
+            /*vm.queries = [
+             {
+             title: 'Parent',
+             text: 'SELECT ?child ?parent WHERE {?child rela:childOf ?parent.}'
+             },
+             {
+             title: 'Spouse',
+             text: 'SELECT ?first ?second WHERE {?first rela:spouseOf ?second}'
+             },
+             {
+             title: 'Grandparent',
+             text: 'SELECT ?person ?grandparent WHERE {?grandparent rela:parentOf ?y .?y rela:parentOf ?person.}'
+             }
+             ];
+             vm.output = 'Text will be here';*/
+            vm.map = {
+                center: {
+                    latitude: 51.262370,
+                    longitude: 46.790909
                 },
-                {
-                    title:'Spouse',
-                    text: 'SELECT ?first ?second WHERE {?first rela:spouseOf ?second}'
-                },
-                {
-                    title:'Grandparent',
-                    text: 'SELECT ?person ?grandparent WHERE {?grandparent rela:parentOf ?y .?y rela:parentOf ?person.}'
-                }
-            ];
-            vm.output = 'Text will be here';*/
-            vm.map = { center: { latitude: 42, longitude: 50 }, zoom: 8 };
+                zoom: 7
+            };
+
+            vm.paths = formatCoords(config.coordArray);
         };
 
-        /*vm.send = function (index) {
-            httpRequest.send('PREFIX rela: <http://purl.org/vocab/relationship/>' + vm.queries[index].text).then(function (response) {
-                console.log(response);
-                vm.output = response.data;
+        function formatCoords(coords) {
+            return _.map(angular.fromJson(coords), function (value) {
+                return [value.latitude, value.longitude];
             });
-        };*/
+        }
+
     }]);
 })();
